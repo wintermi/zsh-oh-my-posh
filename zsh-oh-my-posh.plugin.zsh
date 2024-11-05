9b4df7d969a030f2e53ae0740e57ec7ecd12dfa1
Matthew Winter <33818+wintermi@users.noreply.github.com>
2024-11-05 18:56:27 +1100

    feat: convert theme from 'toml' to 'json'

--- zsh-oh-my-posh.plugin.zsh
+++ zsh-oh-my-posh.plugin.zsh
@@ -9,7 +9,7 @@ fi
 # If the 'POSH_THEME' environment variable is not populated then set
 # the default theme configuration to the one provided with this plugin
 if [[ -z "$POSH_THEME" ]]; then
-    export POSH_THEME="${0:A:h}/theme/ohmyposh.toml"
+    export POSH_THEME="${0:A:h}/theme/ohmyposh.json"
 fi
 
 # Initialise the Oh My Posh Prompt
